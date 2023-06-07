cebdbd09885a8bf1949d1d736d2b94ff4aaea8d8
huqianghui <huqianghui0613@163.com>
2023-06-07 16:33:21 +0800

    add source code

---
+++ interface.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import pandas as pd
+import json
+
+from agent import query_agent, create_agent
+
+
+def decode_response(response: str) -> dict:
+    """This function converts the string response from the model to a dictionary object.
+
+    Args:
+        response (str): response from the model
+
+    Returns:
+        dict: dictionary with response data
+    """
+    return json.loads(response)
+
+
+def write_response(response_dict: dict):
+    """
+    Write a response from an agent to a Streamlit app.
+
+    Args:
+        response_dict: The response from the agent.
+
+    Returns:
+        None.
+    """
+
+    # Check if the response is an answer.
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+
+    # Check if the response is a bar chart.
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.bar_chart(df)
+
+    # Check if the response is a line chart.
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+
+    # Check if the response is a table.
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
+
+
+st.title("👨‍💻 Chat with your data")
+
+st.caption("示例问题如下：")
+
+st.markdown('''1. which book has the highest rating count?   
+
+2. Create a bar graph on the first 5 books. Use the book titles as the column and the rating count as the data values.  
+
+3. tabulate the first 5 books in the table,including the title and rating count columns only.  
+
+4. Create a line graph on the first books. Use the book titles as the column the rating count as the data values.''')
+
+st.subheader("Enter your query below:")
+
+query = st.text_area("")
+
+if st.button("Submit Query", type="primary"):
+    # Create an agent from the CSV file.
+    agent = create_agent()
+
+    # Query the agent.
+    response = query_agent(agent=agent, query=query)
+
+    # Decode the response.
+    decoded_response = decode_response(response)
+
+    # Write the response to the Streamlit app.
+    write_response(decoded_response)
---
+++ mysql_operation.py
@@ -0,0 +1,34 @@
+import mysql.connector
+import pandas as pd
+import streamlit as st
+
+
+mydb = mysql.connector.connect(
+    host="codex-sample-server.mysql.database.azure.com",
+    user="huqianghui",
+    password="hqh@163.com2025",
+    database="codex-sample"
+)
+
+def insert_sql(dataframe):
+    mycursor = mydb.cursor()
+    table_name = "books"
+    columns = ", ".join(dataframe.columns)
+    placeholders = ", ".join(["%s"] * len(dataframe.columns))
+    query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+    values = [tuple(row) for row in dataframe.values]
+    mycursor.executemany(query, values)
+    print("Number of rows inserted: %d" % mycursor.rowcount)
+    mycursor.close()
+    mydb.commit()
+    mydb.close()
+
+
+@st.cache_data
+def read_pd_byTableName(tableName):
+    query = "SELECT * FROM " +  tableName
+    mydb.connect()
+    df = pd.read_sql(query, mydb)
+    mydb.close()
+    return df
+
